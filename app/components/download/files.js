eeff1c5321e639b0825e742d2278de47be7b4ba8
liu <liu@appledeMacBook-Air-2.local>
2018-02-25 13:56:55 +0800

    update files

--- app/components/download/files.js
+++ app/components/download/files.js
@@ -2,7 +2,7 @@ export default [
   {
     icon: require('Image/download-windows.png'),
     title: 'Windows 8或10',
-    version: '版本: 1.3.13',
+    version: '版本: 1.3.13(beta版)',
     updateTime: '更新时间: 2018-2-12',
     url: 'http://www.wisnuc.com/files/wisnuc-setup-1.3.13.exe',
     versionColor: '#03a9f4'
@@ -10,7 +10,7 @@ export default [
   {
     icon: require('Image/download-mac.png'),
     title: 'Mac',
-    version: '版本: 1.3.13',
+    version: '版本: 1.3.13(beta版)',
     updateTime: '更新时间: 2018-2-12',
     url: 'http://www.wisnuc.com/files/WISNUC-1.3.13.dmg',
     versionColor: '#212121'
@@ -18,7 +18,7 @@ export default [
   {
     icon: require('Image/download-android.png'),
     title: 'Android',
-    version: '版本: 1.2.48',
+    version: '版本: 1.2.54(beta版)',
     updateTime: '更新时间: 2018-2-12',
     url: 'http://www.wisnuc.com/files/wisnuc-v1.2.54-release.apk',
     versionColor: '#00c853'
