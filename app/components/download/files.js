2058229fdf7f6a6212c620cdd1a14bfd59aaf18b
liuhua <411981379@qq.com>
2018-09-17 10:28:37 +0800

    update file download

--- app/components/download/files.js
+++ app/components/download/files.js
@@ -2,25 +2,25 @@ export default [
   {
     icon: require('Image/download-windows.png'),
     title: 'Windows 8或10',
-    version: '版本: 1.3.13(beta版)',
+    version: '版本: 1.3.15',
     updateTime: '更新时间: 2018-2-12',
-    url: 'http://www.wisnuc.com/files/wisnuc-setup-1.3.13.exe',
+    url: 'http://www.wisnuc.com/files/wisnuc-setup-1.3.15.exe',
     versionColor: '#03a9f4'
   },
   {
     icon: require('Image/download-mac.png'),
     title: 'Mac',
-    version: '版本: 1.3.13(beta版)',
+    version: '版本: 1.3.15',
     updateTime: '更新时间: 2018-2-12',
-    url: 'http://www.wisnuc.com/files/WISNUC-1.3.13.dmg',
+    url: 'http://www.wisnuc.com/files/WISNUC-1.3.15.dmg',
     versionColor: '#212121'
   },
   {
     icon: require('Image/download-android.png'),
     title: 'Android',
-    version: '版本: 1.2.54(beta版)',
+    version: '版本: 1.3.4',
     updateTime: '更新时间: 2018-2-12',
-    url: 'http://www.wisnuc.com/files/wisnuc-v1.2.54-release.apk',
+    url: 'http://www.wisnuc.com/files/wisnuc-v1.3.4-release.apk',
     versionColor: '#00c853'
   },
 ]
